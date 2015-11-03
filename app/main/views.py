58b63a05f95cd37a68452dc8be2a561785161819
Timeship <leonwowo@gmail.com>
2015-11-03 21:53:28 +0800

    add admin edit profile

--- app/main/views.py
+++ app/main/views.py
@@ -4,9 +4,9 @@ from datetime import datetime
 from flask import render_template,session,redirect,url_for,abort,flash
 from flask.ext.login import login_required,current_user
 from . import main
-from .forms import NameForm,EditProfileForm
+from .forms import NameForm,EditProfileForm,EditProfileAdminForm
 from .. import db
-from ..models import User
+from ..models import User,Role
 
 @main.route('/',methods=['GET','POST'])
 def index():
@@ -43,6 +43,37 @@ def edit_profile():
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
     return render_template('edit_profile.html',form=form)
+
+'''管理员的资料编辑'''
+@main.route('/edit_profile/<int:id>',methods=['GET','POST'])
+@login_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id) #如果提供的id不对则返回404错误，flask-sqlalchemy提供的函数
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        flash('the profile has been updated.')
+        return redirect(url_for('.user',username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile_admin.html',form=form)
+
+
+
+
+
 
 
 
